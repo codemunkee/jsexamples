f432d5238867a317346cef315cc09269bfc8bcfa
Russ Nealis <codemunkee@gmail.com>
2017-12-21 18:12:02 -0800

    short circuit and / if

---
+++ short-circuit-if.js
@@ -0,0 +1,14 @@
+// The  short-circuit conditional evaluation - if
+
+const a = true
+
+// this is essentially an if statement - 'a' will alwways return true,
+// and the second expression executes...
+a && console.log('a is true')
+
+// analog is 
+
+if (a) {
+  console.log('a is true');
+}
+
